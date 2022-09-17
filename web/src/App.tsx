8a484c841ce909339cd70c815a8e295379976145
Pedro Lima <psantoslima86@gmail.com>
2022-09-16 21:31:11 -0300

    developed screen web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,89 @@
+import "./styles/main.css";
+
+import logo from "./assets/Logo.svg";
+import { MagnifyingGlassPlus } from "phosphor-react";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello NLw</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="logo" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-nlw-gradient bg-clip-text text-transparent">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16 ">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 Anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">1 Anúncio</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">CS:GO</strong>
+            <span className="text-zinc-300 text-sm block">7 Anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block">2 Anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortinite</strong>
+            <span className="text-zinc-300 text-sm block">5 Anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              World of Warcraft
+            </strong>
+            <span className="text-zinc-300 text-sm block">2 Anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black">
+              Não achou seu duo?
+            </strong>
+            <span className="text-zinc-400  block">
+              Public um anúncio para encontrar novos players
+            </span>
+          </div>
+          <button
+            className="py-3 px-4 transition duration-300
+           bg-violet-500 text-white rounded hover:bg-violet-700 flex items-center gap-3"
+          >
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
